05f38b44139aef18b5697addc314e3bd0c175eae
kwadrnt <kevinctse@gmail.com>
2017-05-25 12:21:54 -0700

    connected libraries to users

---
+++ app/controllers/library_users_controller.rb
@@ -0,0 +1,8 @@
+class LibraryUsersController < ApplicationController
+
+  def index
+    @user = User.find(params[:user_id])
+    @libraries = @user.libraries # so we type less in the view
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -12,4 +12,7 @@ Rails.application.routes.draw do
   get '/libraries/new', to: 'libraries#new', as: 'new_library'
   post '/libraries', to: 'libraries#create'
   get '/libraries/:id', to: 'libraries#show', as: 'library'
+
+  # libraries user routes
+  get '/users/:user_id/libraries', to: 'library_users#index', as: 'user_libraries'
 end
